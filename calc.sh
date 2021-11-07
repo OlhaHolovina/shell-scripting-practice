4d6741907412fde101f370a3a2ceb471bd840e52
Ola <ol.holovina@gmail.com>
2021-11-06 21:20:13 -0400

    infinite loop add, exiting add, start message add

--- calc.sh
+++ calc.sh
@@ -1,24 +1,28 @@
 #! /usr/bin/env bash
 
 echo "Hello, you are using the best calculator in the world"
-echo "-----------------------------------------------------"
 echo "What would you like to do?"
-echo "press 1, if you want to ADD one number to another"
-echo "press 2, if you want to SUBTRACT one number from another"
-echo "press 3, if you want to EXIT"
-read input
-case $input in
-  "1")
-    echo "sum 2 numbers"
-    ;;
-  "2")
-    echo "subtract 2 numbers"
-    ;;
-  "3")
-    echo "quit"
-    ;;
-  *)
-    echo "error, enter incorrect command!"
-    ;;
-esac
-# When the operation is complete, redisplay the menu.
+while true
+do
+  echo
+  echo "-----------------------  START ---------------------------"
+  echo "press 1, if you want to ADD one number to another"
+  echo "press 2, if you want to SUBTRACT one number from another"
+  echo "press 3, if you want to EXIT"
+  read input
+  case $input in
+    "1")
+      echo "sum 2 numbers"
+      ;;
+    "2")
+      echo "subtract 2 numbers"
+      ;;
+    "3")
+      echo "Exiting..."
+      break
+      ;;
+    *)
+      echo "Error, enter incorrect command!"
+      ;;
+  esac
+done
