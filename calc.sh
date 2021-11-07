8028dfe7acbf8fd51d71aee73cd754c01687ff69
Ola <ol.holovina@gmail.com>
2021-11-06 20:53:36 -0400

    display a menu

--- calc.sh
+++ calc.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
 
 echo "Hello, you are using the best calculator in the world"
-# Then, display a menu that outlines the possible operations:
-  # Add
-  # Subtract
-  # Exit
+echo "-----------------------------------------------------"
+echo "What would you like to do?"
+echo "press 1, if you want to ADD one number to another"
+echo "press 2, if you want to SUBTRACT one number from another"
+echo "press 3, if you want to EXIT"
 # Then, capture the user selection.
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
