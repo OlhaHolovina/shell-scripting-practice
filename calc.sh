28cea1d13bbfd1fb25332349589af4c88efc8e93
Ola <ol.holovina@gmail.com>
2021-11-06 21:00:31 -0400

    capture the user selection

--- calc.sh
+++ calc.sh
@@ -6,7 +6,7 @@ echo "What would you like to do?"
 echo "press 1, if you want to ADD one number to another"
 echo "press 2, if you want to SUBTRACT one number from another"
 echo "press 3, if you want to EXIT"
-# Then, capture the user selection.
+read input
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
